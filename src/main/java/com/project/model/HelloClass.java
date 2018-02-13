76b433898330ba7a653f666a2775a5b4f90e3cbf
jaratchock11 <jaratchock6192035@hotmail.com>
2018-02-13 20:35:40 +0700

    [Add] Model_HelloClass

---
+++ src/main/java/com/project/model/HelloClass.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.project.model;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class HelloClass {
+    private int id;
+    private String message;
+
+    public HelloClass(){
+        
+    }
+    
+    public HelloClass(int id){
+        this.id = id;
+    }
+    
+    public HelloClass(int id, String message) {
+        this.id = id;
+        this.message = message;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloClass{" + "id=" + id + ", message=" + message + '}';
+    }
+    
+
+    
+}
